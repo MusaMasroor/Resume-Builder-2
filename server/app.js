82483238a54afc5041c427106232481764d00a80
Musa Masroor <musamasroor786@gmail.com>
2024-01-03 22:48:57 +0500

    edited cors

--- server/app.js
+++ server/app.js
@@ -8,13 +8,26 @@ dotenv.config({ path: "./config.env" });
 
 const port = process.env.PORT || 5001;
 
-// Set up CORS with specific origin
-const corsOptions = {
-  origin: "*",
-  credentials: true, // Enable credentials (cookies)
-};
+// CORS Configuration
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://euphonious-brigadeiros-4d0865.netlify.app/",
+];
 
-app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  next();
+});
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(require("./routes/auth"));
