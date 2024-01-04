715fe64999a3d062262b9ad5b55079095fe9c54b
Musa Masroor <musamasroor786@gmail.com>
2024-01-05 03:03:50 +0500

    added cors origin

--- server/app.js
+++ server/app.js
@@ -8,7 +8,11 @@ dotenv.config({ path: "./config.env" });
 
 const port = process.env.PORT || 5001;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://gilded-haupia-740589.netlify.app/",
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use(require("./routes/auth"));
